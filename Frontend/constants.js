76e6c4c0a06d215817d3e96896b4da2c05dc5436
Emilia <115099264+EmiliaM152@users.noreply.github.com>
2024-02-04 13:48:26 -0500

    Delete Frontend directory

--- Frontend/constants.js
+++
@@ -1,7 +0,0 @@
-export const axiosInstance = axios.create({
-    baseURL: 'https://localhost:7233/api',
-    headers: {
-      'Content-Type': 'application/json',
-      'Access-Control-Allow-Origin': 'http://localhost:3000',
-    },
-  });
